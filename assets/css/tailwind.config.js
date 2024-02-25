a5a21b70b879b4d66623b26349a7ff3c23487595
Mila Frerichs <mila.frerichs@gmail.com>
2024-02-25 23:31:42 +0100

    initial commit für subject pages for moers

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.{html,js}", "./layouts/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
